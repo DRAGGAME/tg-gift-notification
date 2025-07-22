3351381b316ba93de65ac0577cbb1b7de013cd89
DRAGGAME <maxutnov@mail.ru>
2025-07-22 12:57:54 +0300

    CREATE Dockerfile

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 from aiogram import Bot
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 TG_KEY = os.getenv('TG_API')
 HOST = os.getenv('ip')
